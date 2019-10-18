14056bbc1e3983787388fb0b683a1cce2f95848d
Mkomarr <m.koomarr@gmail.com>
2019-10-18 17:46:48 +0200

    all clients changes done - Only ACF left

--- contact.php
+++ contact.php
@@ -17,10 +17,13 @@
             </section>
 
             <section class="contact-txt">
-                <h4>Napisz do nas</h4>
+                <h4 class="hidden">Napisz do nas</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
                 <h4>Sprawdź także</h4>
-                <p>Facebook | Twitter</p>
+                <p class="sm">
+                    <a href="https://www.facebook.com/Maxi-Media-1870281149965067/" target="_blank"><i class="icon-facebook-squared"></i></a>
+                    <a href="https://twitter.com/MaxiMedia3" target="_blank"><i class="icon-twitter-squared"></i></a>
+                </p>
             </section>
 
             <?php the_content(); ?>
